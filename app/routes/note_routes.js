5caf0a434b9cbe3d28150cc43f7a8ca21bb85eac
Chris Wright <chriswrightft@gmail.com>
2018-03-15 21:32:08 +0000

    updating server and gitignore

---
+++ app/routes/note_routes.js
@@ -0,0 +1,9 @@
+// routes/note_routes.js
+
+module.exports = function(app, db){
+    app.post('/notes', (req, res) => {
+        // you'll create your notes here
+        console.log(req.body);
+        res.send('Hello');
+    });
+}
