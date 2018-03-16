b70a11220d37948d3ba091e0942b5a80a593f3c7
Chris Wright <chriswrightft@gmail.com>
2018-03-16 10:33:14 +0000

    adding in the rest of the CRUD routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express       = require('express');
 const MongoClient   = require('mongodb').MongoClient;
 const bodyParser    = require('body-parser');
+const db            = require('./config/db');
 
 const app           = express();
 
@@ -10,8 +11,13 @@ const port          = 8000;
 
 app.use(bodyParser.urlencoded({ extended: true}));
 
-require('./app/routes')(app, {});
-
-app.listen(port, () => {
+MongoClient.connect(db.url, (err, database) => {
+  if (err) return console.log(err)
+                      
+  // Make sure you add the database name and not the collection name
+  dba = database.db("first_rest_test")
+  require('./app/routes')(app, dba);
+  app.listen(port, () => {
     console.log('We are live on ' + port);
+  });               
 })
